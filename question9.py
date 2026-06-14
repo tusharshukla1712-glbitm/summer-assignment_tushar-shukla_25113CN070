4c4c73ad3393d893735f2f993cec7247cf1d01bc
tusharshukla1712-glbitm <25113cn070@glbitm.ac.in>
2026-06-14 23:53:13 +0530

    Initial commit

---
+++ question9.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not Prime")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime")
