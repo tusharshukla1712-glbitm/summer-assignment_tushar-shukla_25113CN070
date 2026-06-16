0f3a2f1feade39c6e1e0a76168079c264fc41262
tusharshukla1712-glbitm <25113cn070@glbitm.ac.in>
2026-06-16 21:04:22 +0530

    Add Armstrong number checker in question15.py

---
+++ day_4/question15.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number: "))
+
+temp = num
+digits = len(str(num))
+sum = 0
+
+while temp > 0:
+    digit = temp % 10
+    sum += digit ** digits
+    temp //= 10
+
+if sum == num:
+    print("Armstrong Number")
+else:
+    print("Not an Armstrong Number")
