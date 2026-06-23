5c6e098ef34edf21c04b797e891081e383550497
tusharshukla1712-glbitm <25113cn070@glbitm.ac.in>
2026-06-24 00:50:33 +0530

    Add day_11 programs

---
+++ question41.py
@@ -0,0 +1,7 @@
+def find_sum(a, b):
+    return a + b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Sum =", find_sum(num1, num2))
---
+++ question42.py
@@ -0,0 +1,10 @@
+def find_max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Maximum =", find_max(num1, num2))
